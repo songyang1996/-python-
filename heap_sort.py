5bf4d50ab60b4fd3cb816bdf0a00babd99e0f7b5
songyang1996 <levansongyang@gmail.com>
2017-11-24 21:36:08 +0800

    0.4

--- heap_sort.py
+++ heap_sort.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
+# 堆排序 放飞自我的想法是首先创建一个完全二叉树和数组的转换类
 def heap_sort():
     pass
+
+# class Tree(object):
+#     def __init__(self, array):
+#         self.deep = len(array)
+def sift_down(array, node, end):
+    root = node
+    while True:
+        # 调整为最大堆
+        child = 2 * root - 1  # 左节点
+        if child > end:
+            break
+        print("v:", root, array[root], child, array[child])
+        print(array)
+        # 找出两个子节点中较大的 并设置右边的大
+        if child + 1 <= end and array[child] < array[child + 1]:
+            child += 1
+        # 如果最大堆小于较大的child 则交换顺序
+        if array[root] < arr[child]:
+            array[root], array[child] = array[child], array[root]
 
 if __name__ == '__main__':
     heap_sort()
